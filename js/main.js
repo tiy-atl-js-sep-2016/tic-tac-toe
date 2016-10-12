3217bee96166c02e1e7d3a9109372184d5b1ceb3
Brit Butler <redline6561@gmail.com>
2016-10-12 12:07:11 -0400

    Add a toggleColor function and do some styling.

--- js/main.js
+++ js/main.js
@@ -10,3 +10,31 @@ function changeColor (event) {
 // This is jQuery stuff
 var heading = $("#heading");
 heading.click(changeColor);
+
+// This is the same as the above two lines
+// $("#heading").click(changeColor);
+
+var currentPlayer = "X";
+
+function switchPlayer () {
+  if (currentPlayer === "X") {
+    currentPlayer = "O";
+  } else {
+    currentPlayer = "X";
+  }
+}
+
+function toggleColor (event) {
+  var target = event.target;
+  var playerClass;
+
+  if (currentPlayer === "X") {
+    playerClass = "red";
+  } else {
+    playerClass = "blue";
+  }
+
+  target.toggleClass(playerClass);
+}
+
+$(".space").click(toggleColor);
