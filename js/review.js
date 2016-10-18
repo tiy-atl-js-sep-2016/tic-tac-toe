9da7850723e98afa9eab6e8d86b8f9e58a3ca2b2
Brit Butler <redline6561@gmail.com>
2016-10-18 15:27:32 -0400

    Some code and comments from review.

---
+++ js/review.js
@@ -0,0 +1,87 @@
+/*
+   What's the deal with jQuery?
+
+  Install with a build tool? `npm install jquery`
+  Without build tool? Download the js file and use a script tag.
+
+  `$`
+
+ // var panel = document.querySelector("#panel1");
+ // var jqueryPanel = $(panel)
+ // $("#heading")
+
+
+ // addClass, removeClass, toggleClass
+ // jqueryPanel.html(`<p>Hi there</p>`)
+
+ // $.ajax
+
+ // .html - completely replace the inside of this
+ // .append - add to the bottom of this
+
+ */
+
+/*
+
+  Nested data access
+
+   Etsy Search data
+
+   * data.results
+     * Get: img url, title, price, shop name, shop url, item url
+
+  * Instead of generating HTML,
+    make the data I have look like the data I want
+
+*/
+
+function extractUsefulData (etsyItem) {
+  var title = etsyItem.title;
+  var image = etsyItem.Images[0].url_75x75;
+  var storeName = etsyItem.Shop.shop_name;
+  var storeUrl = etsyItem.Shop.url;
+  var price = etsyItem.price;
+  var url = etsyItem.url;
+
+  return {
+    title: title,
+    image: image,
+    store: storeName,
+    storeUrl: storeUrl,
+    url: url,
+    price: price
+  };
+};
+
+// for (var i = 0; i < data.results.length; i++) {
+//   var item = data.results[i];
+//   console.log(extractUsefulData(item));
+// }
+
+// data.results.forEach(function (item) {
+//   console.log(extractUsefulData(item));
+// });
+
+function cheaperThan50Dollars (item) {
+  return Number(item.price) < 50;
+};
+
+var betterData = data.results.map(extractUsefulData);
+var generatedHTML = betterData.map(makeEtsyHtml);
+
+// loop over generatedHTML and add to page
+
+// var filteredItems = [];
+// for (var i = 0; i < betterData.length; i++) {
+//   var item = betterData[i];
+//   if (item.price < 50) {
+//     filteredItems.push(item);
+//   }
+// }
+
+betterData.filter(function (item) {
+  return Number(item.price) < 50;
+});
+
+var cheaperThan50 = betterData.filter(function (x) { return item.price < 50; });
+var costlierThan50 = betterData.filter(function (x) { return item.price > 50; });
